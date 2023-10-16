8ec07e0cde94ae0e406f7071d54defb66dd310ac
pramodbn <begurnagaraj.p@northeastern.edu>
2023-10-15 20:47:42 -0500

    3tier-terraform-module

---
+++ 3tier-terraform-module/modules/webserver/main.tf
@@ -0,0 +1,242 @@
+data "aws_ami" "dpt_ami" {
+  most_recent = true
+  owners      = ["576341600583"]
+
+  filter {
+    name   = "name"
+    values = ["eos-web-*"]
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  workspace = "default"
+  config = {
+    bucket         = "ed-eos-terraform-state"
+    key            = "eos/vpc/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+data "template_cloudinit_config" "init" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = "${file("modules/webserver/scripts/setup.sh")}"
+ }
+
+}
+
+
+resource "aws_launch_configuration" "dpt_lc" {
+  name_prefix   = "${var.application}-web-lc-"
+  image_id             = data.aws_ami.dpt_ami.id
+  key_name     = var.keypair
+  security_groups      = [aws_security_group.dpt_sg.id]
+  user_data_base64     = data.template_cloudinit_config.init.rendered
+  enable_monitoring    = var.enable_monitor
+  ebs_optimized        = var.enable_ebs_optimization
+  instance_type        = var.instance_type
+  iam_instance_profile = var.instance_profile
+
+  root_block_device {
+    volume_type           = var.root_ebs_type
+    volume_size           = var.root_ebs_size
+    delete_on_termination = var.root_ebs_del_on_term
+    
+  }
+
+  associate_public_ip_address = var.associate_public_ip_address
+  
+}
+
+resource "aws_autoscaling_group" "dpt_asg" {
+  name                      = "${var.application}-web-asg"
+  max_size                  = var.asg_max_cap
+  min_size                  = var.asg_min_cap
+  desired_capacity          = var.asg_desired_cap
+  health_check_grace_period = var.asg_health_check_grace_period
+  health_check_type         = var.asg_health_check_type
+  force_delete              = var.asg_force_delete
+  termination_policies      = var.asg_termination_policies
+  suspended_processes       = var.asg_suspended_processes
+  launch_configuration      = aws_launch_configuration.dpt_lc.name
+  vpc_zone_identifier       = [data.terraform_remote_state.vpc.outputs.priv_subnet1]
+  default_cooldown          = var.asg_default_cooldown
+  enabled_metrics           = var.asg_enabled_metrics
+  metrics_granularity       = var.asg_metrics_granularity
+  protect_from_scale_in     = var.asg_protect_from_scale_in
+  target_group_arns         = ["${aws_lb_target_group.webtg1.arn}"]
+  
+  tag {
+    key                 = "application"
+    value               = "${var.application}-dpt"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "dpt-cpu-alarm" {
+  alarm_name          = "dpt-${var.application}-cpu-alarm"
+  alarm_description   = "dpt-${var.application}-cpu-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "80"
+
+  dimensions = {
+    "AutoScalingGroupapplication" = aws_autoscaling_group.dpt_asg.name
+  }
+
+  actions_enabled = true
+  alarm_actions   =  ["${var.notifications_arn}"]
+  ok_actions      =  ["${var.notifications_arn}"]
+}
+
+# Create notifications for the ASG
+resource "aws_autoscaling_notification" "notifications" {
+  group_names = [
+    aws_autoscaling_group.dpt_asg.name,
+  ]
+
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+  ]
+
+  topic_arn = var.notifications_arn
+}
+
+# Create Security Group for  dpt
+resource "aws_security_group" "dpt_sg" {
+  name        = "${var.application}-web-sg"
+  description = "Allow web  traffic"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "target-healthy-count" {
+  alarm_name          = "${var.application}-dpt-tg1-Healthy-Count"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "HealthyHostCount"
+  namespace           = "AWS/ApplicationELB"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "0"
+
+  dimensions = {
+    LoadBalancer = "${aws_lb.dpt.arn_suffix}"
+    TargetGroup  = "${aws_lb_target_group.webtg1.arn_suffix}"
+  }
+
+  alarm_description  = "Trigger an alert when ${var.application}-tg1  has 1 or more unhealthy hosts"
+  alarm_actions      = ["${var.notifications_arn}"]
+  ok_actions         = ["${var.notifications_arn}"]
+}
+
+resource "aws_security_group" "elb" {
+  name        = "${var.application}-web-alb-sg"
+  description = "ALB SG"
+
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  # HTTP access from anywhere
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+resource "aws_lb" "dpt" {
+  name               = "${var.application}-web-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.elb.id}"]
+  subnets            =   [data.terraform_remote_state.vpc.outputs.pub_subnet1,data.terraform_remote_state.vpc.outputs.pub_subnet2]
+
+ tags = {
+      application         = "${var.application}-dpt"
+  }
+}
+
+resource "aws_lb_listener" "web_tg1" {
+  load_balancer_arn = "${aws_lb.dpt.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.webtg1.arn}"
+  }
+}
+
+
+resource "aws_lb_target_group" "webtg1" {
+  name     = "${var.application}-web-tg1"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.vpc.outputs.vpc_id
+
+health_check {
+                path = "/"
+                port = "80"
+                protocol = "HTTP"
+                healthy_threshold = 2
+                unhealthy_threshold = 2
+                interval = 5
+                timeout = 4
+        }
+}
+
+
+resource "aws_route53_record" "dpt" {
+  zone_id = "Z10474391LEWAIHPE933Q"
+  name    = "${var.dns_name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_lb.dpt.dns_name}"
+    zone_id                = "${aws_lb.dpt.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+
---
+++ 3tier-terraform-module/modules/webserver/variables.tf
@@ -0,0 +1,140 @@
+variable "application" {
+  type    = string
+  default = "dpt-web"
+}
+
+variable "keypair" {
+  type    = string
+}
+
+variable "dns_name" {
+  default    = "www"
+}
+
+variable "enable_monitor" {
+  type    = string
+  default = "true"
+}
+
+variable "enable_ebs_optimization" {
+  type    = string
+  default = "false"
+}
+
+variable "notifications_arn" {
+  type = string
+  default = "arn:aws:sns:us-east-1:576341600583:ed-web-alerts"
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "instance_profile" {
+  type    = string
+}
+
+variable "root_ebs_type" {
+  type    = string
+  default = "gp2"
+}
+
+variable "root_ebs_size" {
+  type    = string
+  default = "30"
+}
+
+variable "root_ebs_del_on_term" {
+  type    = string
+  default = "true"
+}
+
+variable "associate_public_ip_address" {
+  type    = string
+  default = "false"
+}
+
+variable "asg_max_cap" {
+  type        = string
+  description = "AWS Autoscaling Group Max Capacity for Dispatcher Nodes"
+}
+
+variable "asg_min_cap" {
+  type        = string
+  description = "AWS Autoscaling Group Min Capacity for Dispatcher Nodes"
+}
+
+variable "asg_desired_cap" {
+  type        = string
+  description = "AWS Autoscaling Group Desired Capacity for Dispatcher Nodes"
+}
+
+variable "asg_health_check_grace_period" {
+  type = string
+
+  default     = "2"
+  description = "AWS Autoscaling Group health check grace period (default: '')"
+}
+
+variable "asg_health_check_type" {
+  type = string
+
+  default     = "EC2"
+  description = "AWS Autoscaling Group health check type (default: 'ELB')"
+}
+
+variable "asg_force_delete" {
+  default     = true
+  description = "AWS Autoscaling Group force delete"
+}
+
+variable "asg_termination_policies" {
+  type = list(string)
+
+  default     = ["Default"]
+  description = "AWS Autoscaling Group termination policies (default: ['Default'])"
+}
+
+variable "asg_suspended_processes" {
+  type = list(string)
+
+  default     = []
+  description = "AWS Autoscaling Group restricted actions (default: [])"
+}
+
+variable "asg_min_elb_cap" {
+  default     = 0
+  description = "Setting this causes Terraform to wait for this number of instances to show up healthy in the ELB only on creation. Updates will not wait on ELB instance number changes (default: '0')"
+}
+
+variable "asg_wait_for_cap" {
+  default     = false
+  description = "Setting this will cause Terraform to wait for exactly this number of healthy instances in all attached load balancers on both create and update operations. Takes precedence over min_elb_capacity behavior."
+}
+
+variable "asg_wait_cap_timeout" {
+  default     = "0"
+  description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out. (See also Waiting for Capacity below.) Setting this to '0' causes Terraform to skip all Capacity Waiting behavior."
+}
+
+variable "asg_default_cooldown" {
+  default     = 60
+  description = "The amount of time, in seconds, after a scaling activity completes before another scaling activity can start"
+}
+
+variable "asg_enabled_metrics" {
+  default = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
+
+  type        = list(string)
+  description = "A list of metrics to collect. The allowed values are GroupMinSize, GroupMaxSize, GroupDesiredCapacity, GroupInServiceInstances, GroupPendingInstances, GroupStandbyInstances, GroupTerminatingInstances, GroupTotalInstances"
+}
+
+variable "asg_metrics_granularity" {
+  default     = "1Minute"
+  description = "The granularity to associate with the metrics to collect. The only valid value is 1Minute"
+}
+
+variable "asg_protect_from_scale_in" {
+  default     = false
+  description = "Allows setting instance protection. The autoscaling group will not select instances with this setting for terminination during scale in events."
+}
