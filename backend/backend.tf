7f59257f86e939a4277d92213f891db5d50fd4e8
pramodbn <begurnagaraj.p@northeastern.edu>
2023-10-17 20:49:35 -0500

    ansible

---
+++ backend/backend.tf
@@ -0,0 +1,25 @@
+# Create S3 Bucket and DynamoDB table to store state and state lock.
+resource "aws_s3_bucket" "bucket" {
+    bucket = "ed-eos-terraform-state"
+
+    object_lock_configuration {
+        object_lock_enabled = "Enabled"
+    }
+    tags = {
+        Name = "S3 Remote Terraform State Store"
+    }
+}
+
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "eos_table"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "DynamoDB Terraform State Lock Table"
+    }
+}
