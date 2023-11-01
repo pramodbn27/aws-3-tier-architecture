b16f484541ff543c141c194a3c0a9d513f38fc38
pramodbn <begurnagaraj.p@northeastern.edu>
2023-10-31 20:52:52 -0500

    vpc

---
+++ vpc/vpc/variable.tf
@@ -0,0 +1,11 @@
+variable "vpcid" {}
+variable "pub_sub1_cidr" {}
+variable "pub_sub1_az" {}
+variable "pub_sub2_cidr" {}
+variable "pub_sub2_az" {}
+variable "rt_destination_cidr" {}
+variable "region" {}
+variable "priv_sub1_cidr" {}
+variable "priv_sub1_az" {}
+variable "priv_sub2_cidr" {}
+variable "priv_sub2_az" {}
